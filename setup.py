34fad35a122eb526f87a8fad9c624aeb322f2035
Saurabh Zingade <48185348+saurabhzingade@users.noreply.github.com>
2020-07-31 00:05:21 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_zincsy',
+      version='0.1',
+      description='Gaussian distributions and Binomial Distributions',
+      packages=['distributions_zincsy'],
+      author_email = 'saurabh.zingade@gmail.com',
+      author='Saurabh Zingade',
+      zip_safe=False)
